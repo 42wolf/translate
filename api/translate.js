976fe70daf2e790555708d827563291982d9f8d3
xushipang <whxumengyang@gmail.com>
2024-08-29 13:29:35 +0800

    add all

--- api/translate.js
+++ api/translate.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-    const { text } = req.body;
+    const { q } = req.body;
     const apiKey = process.env.LINGVANEX_API_KEY;
     const endpoint = 'https://api-gl.lingvanex.com/language/translate/v2';
 
@@ -11,18 +11,17 @@ module.exports = async (req, res) => {
             headers: {
                 'accept': 'application/json',
                 'content-type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
             },
             body: JSON.stringify({
                 target: 'en',
-                q: [text],
-            }),
+                q
+            })
         });
 
         const data = await response.json();
-        res.status(200).json({ translations: data.data.translations });
-
+        res.status(200).json(data);
     } catch (error) {
-        console.error('Error translating:', error);
-        res.status(500).json({ error: 'Translation failed' });
+        res.status(500).json({ error: '翻译失败，请稍后再试。' });
     }
 };
