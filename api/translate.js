272487786d2ebae4c8b53b8e94b82fb5a5b22437
xushipang <whxumengyang@gmail.com>
2024-08-29 12:58:44 +0800

    add js css index

--- api/translate.js
+++ api/translate.js
@@ -2,11 +2,6 @@ const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
     const { text } = req.body;
-
-    if (!text) {
-        return res.status(400).json({ error: 'Missing text' });
-    }
-
     const apiKey = process.env.LINGVANEX_API_KEY;
     const endpoint = 'https://api-gl.lingvanex.com/language/translate/v2';
 
@@ -16,20 +11,18 @@ module.exports = async (req, res) => {
             headers: {
                 'accept': 'application/json',
                 'content-type': 'application/json',
-                'Authorization': `Bearer ${apiKey}`
             },
             body: JSON.stringify({
                 target: 'en',
-                q: [text]
-            })
+                q: [text],
+            }),
         });
 
         const data = await response.json();
-        const translatedText = data.translations[0].translatedText;
-
-        res.status(200).json({ translatedText });
+        res.status(200).json({ translations: data.data.translations });
 
     } catch (error) {
+        console.error('Error translating:', error);
         res.status(500).json({ error: 'Translation failed' });
     }
 };
